2df027912de3109339f96fc9074e30cd7d913f5d
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-02-25 20:28:30 +0900

    레벨 3-2 테스트 코드 연습-2 3번 케이스

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -35,6 +35,11 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
+        // todo.getUser()가 null일 경우를 체크
+        if(todo.getUser()==null){
+            throw new InvalidRequestException("일정을 만든 유저 정보가 존재하지 않습니다.");
+        }
+
         if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
