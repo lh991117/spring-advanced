dc92551375e7f4e8a374928694acc84253db7a06
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-02-25 16:26:26 +0900

    레벨 3-1: 테스트 코드 연습-1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -3,24 +3,25 @@ package org.example.expert.config;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
 class PasswordEncoderTest {
 
-    @InjectMocks
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder = new PasswordEncoder();
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
         // given
         String rawPassword = "testPassword";
+        //받은 rawPassword를 암호화
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        //PasswordEncoder에 있는 matches메서드는 (rawPassword, encodedPassword)를 받는다.
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
